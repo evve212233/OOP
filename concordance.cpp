6b685164d3cb689160e720c86743fd6647d6fab9
evve212233 <zz1456@nyu.edu>
2018-07-10 15:31:58 +0000

    concordance homework

---
+++ concordance.cpp
@@ -0,0 +1,24 @@
+#include "std_lib_facilities.h"
+int main(){
+	vector<string> text;
+	for(string word; cin>>word;){
+		text.push_back(word);
+	}
+	sort(text);
+	cout<<"\nThe sorted text is:\n";
+	for (string w:text){
+		cout<<w<<" ";
+	}
+	cout<<"\n";
+	int count=1;
+	for (int wd=1;wd<text.size();wd++){
+		if (text[wd]==text[wd-1]){
+			count++;
+		}
+		else{
+			cout<<text[wd-1]<<" has "<< count<<" words\n";
+			count=1;
+		}
+	}
+	cout<<text[text.size()-1]<<" has "<<count<<" words\n";
+}
