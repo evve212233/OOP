0bf5396821cf8838c41cd2f934b1893f374cc113
evve212233 <zz1456@nyu.edu>
2018-07-13 15:39:55 +0000

    Token Assignment

--- Token.cpp
+++ Token.cpp
@@ -1,4 +1,5 @@
 #include "std_lib_facilities.h"
+//A token is a sequence of characters that represents something we consider a unit, such 196 as a number or an operator
 class Token{
 public:
 	//two members
@@ -12,27 +13,35 @@ Token get_token(){
 	switch(ch){
 		case ';': case 'q': case '(': case '+':case '-': case '*': case '/': case ')':
 			return Token{ch};
-			//value is default 0;
+			//default value is default 0;
 		case '0': case '1': case '2': case '3':case '4': case '5': case '6': case '7':case '8': case '9':
 			cin.putback(ch);
 			//putback
 			double val;
 			cin>>val;
 			return Token{'8',val};
+		default:
+		    return Token{'x',double(ch)};
 	}
 };
 vector <Token> tokens;
 int main(){
-	Token t=get_token();
-	while (t.kind!='q'){
-		tokens.push_back(t);
-		t=get_token();
-	}
+//	Token t=get_token();
+for(Token t=get_token(); t.kind!='q'; t=get_token()){
+	tokens.push_back(t);
+}
+//	while (t.kind!='q'){
+//		tokens.push_back(t);
+//		t=get_token();
+//	}
+
 	for (Token tok:tokens){
 		if(tok.kind=='8')
-			cout<<"a number with val is "<<tok.value<<"\n";
+			cout<<"A number token with val = "<<tok.value<<"\n";
+		else if (tok.kind=='x')
+		    cout<<"we received an invalid token value of "<<char(tok.value)<<"\n";
 		else
-			cout<<"an token with expression is "<<tok.kind<<"\n";
+			cout<<"A token of kind "<<tok.kind<<"\n";
 	}
 
 }
