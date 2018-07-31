86b130f0db20f7a6c90bf4c559812781beb11840
evve212233 <35902333+evve212233@users.noreply.github.com>
2018-07-31 11:02:07 -0400

    pointer assignment

--- pointer/pointer_test.cpp
+++ pointer/pointer_test.cpp
@@ -5,13 +5,13 @@ int main(){
 	double d=3.14;
 	char*cp=&c;
 	int*ip=&i;
-	double*dd=&d;
+	double*dp=&d;
 
 	cout<<"cp="<<cp<<" and its value is "<<*cp<<"\n";
 	cout<<"ip="<<ip<<" and its value is "<<*ip<<"\n";
-	cout<<"dd="<<dd<<" and its value is "<<*dd<<"\n";
+	cout<<"dp="<<dp<<" and its value is "<<*dp<<"\n";
 
 	cout<<"size of cp is "<<sizeof(cp)<<", size of c is "<<sizeof(c)<<"\n";
 	cout<<"size of ip is "<<sizeof(ip)<<", size of i is "<<sizeof(i)<<"\n";
-	cout<<"size of dd is "<<sizeof(dd)<<", size of d is "<<sizeof(d)<<"\n";
+	cout<<"size of dp is "<<sizeof(dp)<<", size of d is "<<sizeof(d)<<"\n";
 }
