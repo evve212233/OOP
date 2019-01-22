fc6bbb890c24a114e1aacfd3b4634d7f7717cb4a
Ava Zheng <35902333+evve212233@users.noreply.github.com>
2019-01-22 18:59:13 -0500

    Movie proj made for practice oop

---
+++ movie_proj/Movie.cpp
@@ -0,0 +1,32 @@
+/******************************************************************
+ * Section 13 Challenge
+ * Movie.cpp
+ * 
+ * Models a Movie with the following atttributes
+ * 
+ * std::string name - the name of the movie 
+ * std::string rating - G, PG, PG-13, R
+ * int watched - the number of times you've watched the movie
+ * ***************************************************************/
+#include <iostream>
+#include "Movie.h"
+
+
+
+Movie::Movie(std::string name, std::string rating, int watched) 
+    : name(name), rating(rating), watched(watched)  {
+}
+
+
+Movie::Movie(const Movie &source) 
+    : Movie{source.name, source.rating, source.watched} {
+}
+
+
+Movie::~Movie() {
+}
+
+
+void Movie::display() const {
+    std::cout << name << ", " << rating <<  ", " << watched  <<   std::endl;
+}
